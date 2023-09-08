a5270484027fb5b6766adf623b71a25f283cd4ec
illgamhoduck <hyunbyung87@gmail.com>
2023-09-09 06:32:32 +0900

    feat: configuration interface to list/load configs

---
+++ dataverse/config/interface.py
@@ -0,0 +1,7 @@
+"""
+Interface to check & load the configurations for installation environment
+
+Could consider naming each configuration and load them separately
+awesome_config = dataverse.load_config("ducky_awesome_config")
+"""
+
