e7f1b74587ba0e87feee6e33befc676716235a91
illgamhoduck <hyunbyung87@gmail.com>
2023-09-20 07:29:25 +0900

    feat: ETL data ingestion - ufl

---
+++ dataverse/etl/data_ingestion/ufl.py
@@ -0,0 +1,18 @@
+
+"""
+Load UFL (Upstage Format for LLM) data
+"""
+
+import os
+from dataverse.etl.registry import register_etl
+
+
+@register_etl
+def data_ingestion___ufl___parquet2ufl(spark, input_paths, repartition=20, *args, **kwargs):
+    """
+    load ufl saved as parquet
+    """
+    df = spark.read.parquet(*input_paths)
+    rdd = df.rdd.repartition(repartition)
+    rdd = rdd.map(lambda row: row.asDict())
+    return rdd
