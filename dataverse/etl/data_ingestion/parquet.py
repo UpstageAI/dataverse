e8eb0fd903c9bf528c540c1faa01e0ead41931f6
illgamhoduck <hyunbyung87@gmail.com>
2023-11-03 04:13:52 +0900

    refactor: ETL data ingestion rename ufl to parquet

--- dataverse/etl/data_ingestion/parquet.py
+++ dataverse/etl/data_ingestion/parquet.py
@@ -1,6 +1,5 @@
 
 """
-Load UFL (Upstage Format for LLM) data
 """
 
 import os
@@ -8,10 +7,7 @@ from dataverse.etl import register_etl
 
 
 @register_etl
-def data_ingestion___ufl___parquet2ufl(spark, input_paths, repartition=20, *args, **kwargs):
-    """
-    load ufl saved as parquet
-    """
+def data_ingestion___parquet___pq2ufl(spark, input_paths, repartition=20, *args, **kwargs):
     df = spark.read.parquet(*input_paths)
     rdd = df.rdd.repartition(repartition)
     rdd = rdd.map(lambda row: row.asDict())
