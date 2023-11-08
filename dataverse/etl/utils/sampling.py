de940c4a10927f56ac0bf61393a910ebd0da3363
illgamhoduck <hyunbyung87@gmail.com>
2023-11-08 12:02:58 +0000

    style: ETL code to camel case

--- dataverse/etl/utils/sampling.py
+++ dataverse/etl/utils/sampling.py
@@ -9,7 +9,7 @@ from dataverse.etl import register_etl
 @register_etl
 def utils___sampling___random(
     ufl: RDD,
-    withReplacement=False,
+    replace=False,
     sample_n_or_frac=0.1,
     seed=42,
     *args,
@@ -20,14 +20,14 @@ def utils___sampling___random(
 
     Args:
         ufl: ufl rdd
-        withReplacement: with replacement
+        replace: with replacement
         sample_n_or_frac: sample_n or fraction
         seed: seed
     """
     if isinstance(sample_n_or_frac, float):
-        ufl = ufl.sample(withReplacement, sample_n_or_frac, seed)
+        ufl = ufl.sample(replace, sample_n_or_frac, seed)
 
     # XXX: Take too long, 1M sample takes over 10 mins and didn't finish
     elif isinstance(sample_n_or_frac, int):
-        ufl = ufl.takeSample(withReplacement, sample_n_or_frac, seed)
+        ufl = ufl.takeSample(replace, sample_n_or_frac, seed)
     return ufl
