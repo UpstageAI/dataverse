885041bc26486f77b6bbaff054c80229a43614d9
41ow1ives <77601251@daum.net>
2024-03-05 11:30:10 +0000

    docs: modify docstring, add license, typo in cleaning (#12)

--- dataverse/etl/cleaning/html.py
+++ dataverse/etl/cleaning/html.py
@@ -1,34 +1,46 @@
+"""
+A collection of modules for cleaning data includes html.
 
+Copyright (c) 2024-present Upstage Co., Ltd.
+Apache-2.0 license
+"""
 
+from typing import Union
+
+import html2text
 from pyspark.rdd import RDD
 from pyspark.sql import DataFrame
 
 from dataverse.etl.registry import register_etl
-
-import html2text
-
-from typing import Union
 
 
 @register_etl
 def cleaning___html___extract_plain_text(
     spark,
     data: Union[RDD, DataFrame],
-    subset='text',
-    use_trafilatura=False,
+    subset: str = "text",
+    use_trafilatura: bool = False,
     *args,
     **kwargs
-):
-    """
-    extract plain text from html
+) -> RDD:
+    r"""
+    Extracts plain text from HTML.
 
-    [ caveat ]
-    - `html2text` addes double newline after each paragraph
-        - we are not handling it at this point
-    - why I've put `trafilatura` as an option?
-        - because the extracting plain text with `trafilatura` does not seems to work well
-            - [ OK ] case
-                ```python
+    Args:
+        spark (SparkSession): The Spark session object.
+        data (Union[RDD, DataFrame]): The input data to be processed.
+        subset (str, optional): A subset or column to consider. Defaults to 'text'.
+        use_trafilatura (bool, optional): Whether to use trafilatura instead of html2text. Defaults to False.
+
+    Returns:
+        The plain data extracted from html.
+
+    Caveats:
+        - ``html2text`` adds a double newline after each paragraph, which is not handled at this point.
+        - The option to use `trafilatura` is provided because extracting plain text with ``trafilatura`` does not seem to work well in some cases.
+
+            - [OK] Case::
+
                 text = "<body><h1>My First Heading</h1><p>My first paragraph.</p></body>"
 
                 # html2text
@@ -38,9 +50,9 @@ def cleaning___html___extract_plain_text(
                 # trafilatura
                 print(trafilatura.html2txt(text))
                 >>> 'My First HeadingMy first paragraph.'
-                ```
-            - [ ERROR ] case (trafilatura removes all the text)
-                ```python
+
+            - [ERROR] Case (trafilatura removes all the text)::
+
                 text = "<p>hello <br> nice to meet you.</p>"
 
                 # html2text
@@ -50,11 +62,6 @@ def cleaning___html___extract_plain_text(
                 # trafilatura
                 print(trafilatura.html2txt(text))
                 >>> ''
-                ```
-
-    args:
-        subset (str): subset or columns to consider
-        use_trafilatura (bool): use trafilatura instead of html2text
     """
     if isinstance(data, DataFrame):
         data = data.rdd
@@ -63,10 +70,13 @@ def cleaning___html___extract_plain_text(
     # this is optional
     if use_trafilatura:
         import trafilatura
+
         def _html2txt(row):
             row[subset] = trafilatura.html2txt(row[subset])
             return row
+
     else:
+
         def _html2txt(row):
             row[subset] = html2text.html2text(row[subset])
             return row
