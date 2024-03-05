c6291575ac36d1b18e3318f52efb08b30c74251d
41ow1ives <77601251@daum.net>
2024-03-05 12:08:49 +0000

    docs: modify docstring, add license, typo in pii (#12)

--- dataverse/etl/pii/card.py
+++ dataverse/etl/pii/card.py
@@ -1,65 +1,71 @@
+"""
+Copyright (c) 2024-present Upstage Co., Ltd.
+Apache-2.0 license
+"""
 
+import random
+import re
+from typing import Union
 
 from pyspark.rdd import RDD
 from pyspark.sql import DataFrame
 
 from dataverse.etl.registry import register_etl
 
-import re
-import random
-from typing import Union
-
-
 
 @register_etl
 def pii___card___replace_card_number(
     spark,
     data: Union[RDD, DataFrame],
-    subset='text',
-    pattern=r'(\d{4}-\d{4}-\d{4}-\d{4})',
-    random_pii=True,
-    replace_pii=False,
-    replace_token='[CARD_NUMBER]',
-    start_token='',
-    end_token='',
+    subset: str = "text",
+    pattern: str = r"(\d{4}-\d{4}-\d{4}-\d{4})",
+    random_pii: bool = True,
+    replace_pii: bool = False,
+    replace_token: str = "[CARD_NUMBER]",
+    start_token: str = "",
+    end_token: str = "",
     *args,
-    **kwargs
-):
-    """
+    **kwargs,
+) -> RDD:
+    r"""
     Replace card number with a random number or a token
 
-    `replace_pii` takes precedence over `random_pii`
-    - e.g when both are True, the card number will be replaced with the token
-    - e.g. this is 1234-1234-1234-1234 -> this is [CARD_NUMBER]
+    Args:
+        spark: The SparkSession object.
+        data (Union[RDD, DataFrame]): The input data to process.
+        subset (str, optional): The subset or columns to consider. Defaults to 'text'.
+        pattern (str, optional): The regex pattern to find. Defaults to r'(\d{4}-\d{4}-\d{4}-\d{4})'.
+        random_pii (bool, optional): If True, replace the pii with a random number. Defaults to True.
+        replace_pii (bool, optional): If True, replace the pii with the `replace_token`. Defaults to False.
+        replace_token (str, optional): The token to replace the pii with. Defaults to '[CARD_NUMBER]'.
+        start_token (str, optional): The start token to append where the pattern is found. Defaults to ''.
+        end_token (str, optional): The end token to append where the pattern is found. Defaults to ''.
 
-    `start_token` and `end_token` are used to append the token to the start and end of the card number
-    - it doens't matter with `random_card_number` or `replace_card_number` is True or False
+    Returns:
+        RDD: The processed data.
 
-    [ example ]
-    <input>
-        - text = 'card number is 1234-1234-1234-1234.'
+    Caveats:
+        - `replace_pii` takes precedence over `random_pii`
+            - e.g when both are True, the card number will be replaced with the token
+            - e.g. this is 1234-1234-1234-1234 -> this is [CARD_NUMBER]
+        - `start_token` and `end_token` are used to append the token to the start and end of the card number
+            - it doens't matter with `random_card_number` or `replace_card_number` is True or False
 
-    <output>
-        - random pii
-            - text = 'card number is 2238-1534-1294-1274.'
-        - replace pii
-            - replace_token = '[CARD_NUMBER]'
-            - text = 'card number is [CARD_NUMBER].'
-        - start token
-            - start_token = '[CARD_NUMBER_START]'
-            - text = 'card number is [CARD_NUMBER_START]1234-1234-1234-1234.'
-        - end token
-            - end_token = '[CARD_NUMBER_END]'
-            - text = 'card number is 1234-1234-1234-1234[CARD_NUMBER_END].'
+    Examples:
+        <input>
+            - text = 'card number is 1234-1234-1234-1234.'
 
-    args:
-        subset (str): subset or columns to consider
-        pattern (str): regex pattern to find
-        random_pii (bool): if True, replace the pii with a random number
-        replace_pii (bool): if True, replace the pii with the `replace_token`
-        replace_token (str): token to replace the pii with
-        start_token (str): start token to append where the pattern is found
-        end_token (str): end token to append where the pattern is found
+        <output>
+            - random pii
+                - text = 'card number is 2238-1534-1294-1274.'
+            - replace pii
+                - replace_token = '[CARD_NUMBER]'
+                - text = 'card number is [CARD_NUMBER].'
+            - start token
+                - start_token = '[CARD_NUMBER_START]'
+                - text = 'card number is [CARD_NUMBER_START]1234-1234-1234-1234.'
+            - end token
+                - end_token = '[CARD_NUMBER_END]'
     """
     if isinstance(data, DataFrame):
         data = data.rdd
@@ -70,7 +76,7 @@ def pii___card___replace_card_number(
         if replace_pii:
             match = replace_token
         elif random_pii:
-            match = re.sub('\d', lambda x: str(random.randint(0, 9)), match)
+            match = re.sub(r"\d", lambda x: str(random.randint(0, 9)), match)
 
         return f"{start_token}{match}{end_token}"
 
