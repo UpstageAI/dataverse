9a5f1a0c91db7923665275f5586f8cf59aa37630
41ow1ives <77601251@daum.net>
2024-03-05 12:17:20 +0000

    docs: modify docstring, add license, typo in utils (#12)

--- dataverse/etl/utils/log.py
+++ dataverse/etl/utils/log.py
@@ -1,32 +1,34 @@
+"""
+Copyright (c) 2024-present Upstage Co., Ltd.
+Apache-2.0 license
+"""
 
+from typing import Union
 
 from pyspark.rdd import RDD
 from pyspark.sql import DataFrame
-
-from typing import Union, List, Tuple
-from operator import add
 
 from dataverse.etl import register_etl
 
 
 @register_etl
 def utils___log___count(
-    spark,
-    data: Union[RDD, DataFrame],
-    prev_etl_name=None,
-    *args,
-    **kwargs
-) -> RDD[List[Tuple[str, int]]]:
+    spark, data: Union[RDD, DataFrame], prev_etl_name: str = None, *args, **kwargs
+) -> Union[RDD, DataFrame]:
     """
     Simply count the number of rows in the data
 
     Args:
-        spark (SparkSession): SparkSession
-        data (Union[RDD, DataFrame]): data to extract the nouns from
+        spark (SparkSession): The Spark session object.
+        data (Union[RDD, DataFrame]): The input data to extract the nouns from.
+        prev_etl_name (str, optional): name of the previous ETL process. Defaults to None.
+
+    Returns:
+        Union[RDD, DataFrame]: The input data. Nothing is changed.
     """
     total_data = data.count()
-    print('=' * 50)
+    print("=" * 50)
     print(f"After [ {prev_etl_name} ] - Total data: {total_data}")
-    print('=' * 50)
+    print("=" * 50)
 
     return data
