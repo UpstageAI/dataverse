968a20d8a7662441648c3fba35a6c0ca9ab13469
41ow1ives <77601251@daum.net>
2024-03-05 11:30:10 +0000

    docs: modify docstring, add license, typo in cleaning (#12)

--- dataverse/etl/cleaning/document.py
+++ dataverse/etl/cleaning/document.py
@@ -1,49 +1,76 @@
+"""
+A collection of modules for cleaning data at the document level.
 
+Copyright (c) 2024-present Upstage Co., Ltd.
+Apache-2.0 license
+"""
+
+from typing import Union
 
 from pyspark.rdd import RDD
 from pyspark.sql import DataFrame
 
 from dataverse.etl.registry import register_etl
 
-import re
-import unicodedata
-from typing import Union
-
 
 @register_etl
 def cleaning___document___split_by_word(
     spark,
     data: Union[RDD, DataFrame],
-    subset='text',
-    word_per_chunk=100,
-    delimiter=" ",
+    subset: str = "text",
+    word_per_chunk: int = 100,
+    delimiter: str = " ",
     *args,
     **kwargs
-):
+) -> RDD:
     """
-    split documents to smaller chunks by word
+    Split documents into smaller chunks by word.
 
-    [ example ]
-    - word_per_chunk = 2
-    - delimiter = " "
-    - text = "hello world, how are you?"
-    - result = ["hello world,", "how are", "you?"]
+    Args:
+        spark (SparkSession): The Spark session object.
+        data (Union[RDD, DataFrame]): The input data to be processed.
+        subset (str, optional): A subset or column to consider. Defaults to 'text'.
+        word_per_chunk (int, optional): Number of words per chunk. Defaults to 100.
+        delimiter (str, optional): Delimiter to split the text. Defaults to " ".
 
-    [ caveat ]
-    - NO normalization is done here!
-        - this doesn't consider the whitespace normalization
-        - recommend to use other normalization before this
-    - All the keys from the original row are copied to all the new rows created
-        - `id` is not unique anymore
-        - make sure `id` is assigned after this step
+    Returns:
+        RDD: The processed data with documents split into smaller chunks.
 
-    args:
-        subset (str): subset or columns to consider
-        word_per_chunk (int): number of words per chunk
-        delimiter (str): delimiter to split the text
-            - default: " "
-            - decide the unit of word
+    Raises:
+        ValueError: If word_per_chunk is not a positive integer.
+
+    Examples:
+        - word_per_chunk = 2
+        - delimiter = " "
+        - input
+
+            +-----------------------------+
+            |            text             |
+            +=============================+
+            | "hello world, how are you?" |
+            +-----------------------------+
+
+        - output
+
+            +----------------+
+            |      text      |
+            +================+
+            | "hello world," |
+            +----------------+
+            | "how are"      |
+            +----------------+
+            | "you?"         |
+            +----------------+
+
+    Caveats:
+        - NO normalization is done here!
+            - This doesn't consider the whitespace normalization.
+            - Recommend using other normalization before this.
+        - All the keys from the original row are copied to all the new rows created.
+            - ``id`` is not unique anymore.
+            - Make sure ``id`` is assigned after this step.
     """
+
     if isinstance(data, DataFrame):
         data = data.rdd
         data = data.map(lambda row: row.asDict())
@@ -54,7 +81,7 @@ def cleaning___document___split_by_word(
         # Create chunks
         chunks = []
         for i in range(0, len(words), word_per_chunk):
-            chunks.append(delimiter.join(words[i: i+word_per_chunk]))
+            chunks.append(delimiter.join(words[i : i + word_per_chunk]))
 
         # Create a new dictionary for each chunk with all the keys from the original row
         return [{**row, subset: chunk} for chunk in chunks]
--- dataverse/etl/cleaning/table.py
+++ dataverse/etl/cleaning/table.py
@@ -16,47 +16,58 @@ from dataverse.etl.registry import register_etl
 def cleaning___table___merge_col_vertical(
     spark,
     data: Union[RDD, DataFrame],
-    col1=None,
-    col2=None,
-    merge_col_name="merge_col",
+    col1: str = None,
+    col2: str = None,
+    merge_col_name: str = "merge_col",
     *args,
     **kwargs
-):
+) -> RDD:
     """
-    merge 2 column vertically to 1 column
-    what does this mean? here is example
+    Merges two columns vertically into one column.
 
-    [ before ]
-    -----------------------
-    col1 col2 species
-    1    2    duck
-    3    4    duck
-    5    6    ducky
-    -----------------------
+    Example:
+        Before:
 
-    cleaning___table___merge_col_vertical(
-        ...,
-        col1="col1",
-        col2="col2",
-        merge_col_name="number",
-        ...
-    )
+        +------+------+---------+
+        | col1 | col2 | species |
+        +======+======+=========+
+        | 1    | 2    | duck    |
+        +------+------+---------+
+        | 3    | 4    | duck    |
+        +------+------+---------+
+        | 5    | 6    | ducky   |
+        +------+------+---------+
 
-    [ after ]
-    -----------------------
-    number species
-    1      duck
-    2      duck
-    3      duck
-    4      duck
-    5      ducky
-    6      ducky
-    -----------------------
+        After calling ``cleaning_table_merge_col_vertical(...)``:
 
-    args:
-        col1: column to merge
-        col2: column to merge
-        merge_col_name: name of merged column
+        +--------+---------+
+        | number | species |
+        +========+=========+
+        | 1      | duck    |
+        +--------+---------+
+        | 3      | duck    |
+        +--------+---------+
+        | 5      | ducky   |
+        +--------+---------+
+        | 2      | duck    |
+        +--------+---------+
+        | 4      | duck    |
+        +--------+---------+
+        | 6      | ducky   |
+        +--------+---------+
+
+    Args:
+        spark (SparkSession): The Spark session object.
+        data (Union[RDD, DataFrame]): The input data to be processed. It can be either an RDD or a DataFrame.
+        col1 (str): The name of the first column to merge.
+        col2 (str): The name of the second column to merge.
+        merge_col_name (str, optional): The name of the merged column.
+
+    Returns:
+        The processed data with the merged column.
+
+    Raises:
+        ValueError: If col1 or col2 is not specified.
     """
     if isinstance(data, RDD):
         data = data.toDF()
