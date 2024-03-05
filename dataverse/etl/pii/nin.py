1e89d9559ea4052a74598e0f896d9fc9976a2edd
41ow1ives <77601251@daum.net>
2024-03-05 12:08:49 +0000

    docs: modify docstring, add license, typo in pii (#12)

--- dataverse/etl/pii/nin.py
+++ dataverse/etl/pii/nin.py
@@ -1,4 +1,3 @@
-
 """
 NIN (National Identification Number)
 =====================================
@@ -9,65 +8,74 @@ and temporary residents for the purposes of work, taxation,
 government benefits, health care, and other governmentally-related functions.
 
 https://en.wikipedia.org/wiki/National_identification_number
+
+Copyright (c) 2024-present Upstage Co., Ltd.
+Apache-2.0 license
 """
+
+import random
+import re
+from typing import Union
 
 from pyspark.rdd import RDD
 from pyspark.sql import DataFrame
 
 from dataverse.etl.registry import register_etl
 
-import re
-import random
-from typing import Union
-
-
 
 @register_etl
 def pii___nin___replace_korean_rrn(
     spark,
     data: Union[RDD, DataFrame],
-    subset='text',
-    pattern=r'\d{6}-\d{7}',
-    random_pii=True,
-    replace_pii=False,
-    replace_token='[NIN]',
-    start_token='',
-    end_token='',
+    subset: str = "text",
+    pattern: str = r"\d{6}-\d{7}",
+    random_pii: bool = True,
+    replace_pii: bool = False,
+    replace_token: str = "[NIN]",
+    start_token: str = "",
+    end_token: str = "",
     *args,
-    **kwargs
-):
-    """
+    **kwargs,
+) -> RDD:
+    r"""
     Replace Korean RRN (Resident Registration Number) with a random number or a token
 
-    `replace_pii` takes precedence over `random_pii`
-    `start_token` and `end_token` are used to append the token to the start and end of the number
-    - it doens't matter with `random_pii` or `replace_pii` is True or False
+    Args:
+        spark (SparkSession): The Spark session object.
+        data(Union[RDD, DataFrame]): The input data to be processed.
+        subset(str, optional): A subset or column to consider. Defaults to 'text'.
+        pattern(str, optional): The regex pattern to find. Defaults to r'\d{6}-\d{7}'.
+        random_pii(str, optional): If True, replace the pii with a random number. Defaults to True.
+        replace_pii(bool, optional): If True, replace the pii with the `replace_token`. Defaults to False.
+        replace_token(bool, optional): The token to replace the pii with. Defaults to '[NIN]'.
+        start_token(str, optional): The start token to append where the pattern is found. Defaults to ''.
+        end_token(str, optional): The end token to append where the pattern is found. Defaults to ''.
 
-    [ example ]
-    <input>
-        - text = 'nin is 123456-1234567'
+    Returns:
+        rdd: The processed data with replaced Korean RRN.
 
-    <output>
-        - random pii 
-            - text = 'nin is 141124-1244121'
-        - replace pii
-            - replace_token = '[NIN]'
-            - text = 'nin is [NIN].'
-        - start token
-            - start_token = '[NIN_START]'
-            - text = 'nin is [NIN_START]123456-1234567'
-        - end token
-            - end_token = '[NIN_END]'
-            - text = 'nin is 123456-1234567[NIN_END].'
+    Caveats:
+        - `replace_pii` takes precedence over `random_pii`
+        - `start_token` and `end_token` are used to append the token to the start and end of the number
+            - it doens't matter with `random_pii` or `replace_pii` is True or False
 
-    args:
-        subset (str): subset or columns to consider
-        pattern (str): regex pattern to find
-        random_pii (bool): if True, replace the pii with a random number
-        replace_pii (bool): if True, replace the pii with the `replace_token`
-        replace_token (str): token to replace the pii with
-        start_token (str): start token to append where the pattern is found
-        end_token (str): end token to append where the pattern is found
+
+    Examples:
+        <input>
+            - text = 'nin is 123456-1234567'
+
+        <output>
+            - random pii
+                - text = 'nin is 141124-1244121'
+            - replace pii
+                - replace_token = '[NIN]'
+                - text = 'nin is [NIN].'
+            - start token
+                - start_token = '[NIN_START]'
+                - text = 'nin is [NIN_START]123456-1234567'
+            - end token
+                - end_token = '[NIN_END]'
+                - text = 'nin is 123456-1234567[NIN_END].'
     """
     if isinstance(data, DataFrame):
         data = data.rdd
@@ -78,7 +86,7 @@ def pii___nin___replace_korean_rrn(
         if replace_pii:
             match = replace_token
         elif random_pii:
-            match = re.sub('\d', lambda x: str(random.randint(0, 9)), match)
+            match = re.sub(r"\d", lambda x: str(random.randint(0, 9)), match)
 
         return f"{start_token}{match}{end_token}"
 
